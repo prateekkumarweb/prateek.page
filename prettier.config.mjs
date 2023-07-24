6abcc272ecbefb16d5ca85982bb6fea9ddec7425
Prateek Kumar <me@prateek.page>
2023-07-24 17:02:48 +0530

    update deps, fmt code

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Options} */
+export default {
+  plugins: [import("prettier-plugin-astro")],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
