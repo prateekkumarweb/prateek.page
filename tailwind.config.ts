8f6d6093ca23e79a8db624f3991ef5e348ab702a
Prateek Kumar <me@prateek.page>
2024-11-06 22:33:37 +0530

    add bluesky link

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
@@ -6,4 +7,4 @@ export default {
     extend: {},
   },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
-};
+} satisfies Config;
